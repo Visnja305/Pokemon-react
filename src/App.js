8575dc79e56ae05484beb4799951cd5503593a93
Visnja305 <105449754+Visnja305@users.noreply.github.com>
2023-10-16 15:42:10 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import Showcase from "./Showcase";
+import BaseStats from "./BaseStats";
+import PokeMove from "./PokeMove";
+import "./App.css";
+
 function App() {
+  const baseStats = {
+    hp: 45,
+    attack: 49,
+    defense: 48,
+    spAttack: 65,
+    spDef: 66,
+    speed: 47
+  };
+
+  const handleClick = () => {
+    alert(
+      `Special Stats:\n \tSpecial Attack: ${baseStats.spAttack}\n\tSpecial Defense: ${baseStats.spDef}`
+    );
+  };
   return (
-    <div>
+    <div className="main-wrapper background">
       <h1>App Component</h1>
+      <Showcase />
+      <BaseStats clicker={handleClick} stats={baseStats} />
+      <PokeMove />
     </div>
   );
 }
-aaaaaaaa
+
 export default App;
